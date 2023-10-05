58603d124006ab5fbaed68d62581193360a7b526
juanppalacios <palaciosvilchis.jpa@gmail.com>
2023-10-04 21:59:03 -0400

    removed file

--- src/main.py
+++ src/main.py
@@ -6,35 +6,33 @@ from numba import jit, cuda
 from rich.progress import track
 from notebook_utils import *
 
-# message level set to debugging mode
-configure_logging(logging.DEBUG)
+'''
+     _      _                                _
+    | |    (_)                              | |
+  __| |_ __ ___   _____ _ __    ___ ___   __| | ___
+ / _` | '__| \ \ / / _ \ '__|  / __/ _ \ / _` |/ _ \
+| (_| | |  | |\ V /  __/ |    | (_| (_) | (_| |  __/
+ \__,_|_|  |_| \_/ \___|_|     \___\___/ \__,_|\___|
+'''
 
 # read in our training and testing data sets
 train_rna = read_input('../train/training_set_rna.csv')
 train_adt = read_input('../train/training_set_adt.csv')
 test_rna  = read_input('../test/test_set_rna.csv')
 
-# creating our predicted testing ADT set
-test_adt  = create_matrix(train_adt['height'], test_rna['width'])
+# note: use only with DEBUG_MODE
+gold_adt  = read_input('../test/test_set_rna.csv')
 
-# x = linear_solve(train_rna, train_adt)
+#> hyper-parameter test case lists
+parameter_0 = ['logarithm', 'clip', 'normalize'] #> pre-process methods
+parameter_1 = [linear_regression, gradient_descent, nmf] #> solution methods
 
-#> selecting our model hyper-parameters
-models    = ['linear_regression', 'NMF', 'SVD', ]
-parameters = [1, 2, 3]
+model = Model()
 
-test_cases = [{} for _ in range(len(models) * len(parameters))]
+model.configure(debug_mode = True, test_cases = [parameter_0, parameter_1])
 
-for test_case in track(test_cases):
-    # debug(f"\nrunning test case {test_case}\n")
-    # debug(f"fitting our model with x_train y_train")
-    # debug(f"evaluate model and save score to test case list")
-    time.sleep(2)
+model.fit(train_rna, train_adt)
 
-#> model.fit(train_rna, train_adt)
+test_adt = model.predict(test_rna, gold_adt)
 
-# predict
-# test_adt = predict_using(test_rna)
-
-# write our output
 write_output('../out/debug/kaggle_challenge_2.csv', test_adt['data'])
